d9c845bb2b91de814ef4503ea1e89d7a5b6a1e28
kecramer <kcramer@gmail.com>
2018-07-10 15:11:45 -0700

    Remove log message

--- controller/dxcc.js
+++ controller/dxcc.js
@@ -4,7 +4,6 @@ const dxccjs = require('dxccjs')
 let dxcc = new dxccjs('./data/cty.xml')
 
 const show = (req, res) => {
-  console.log(req.params.call)
   let dxccEntity = dxcc.checkCall(req.params.call)
   res.json(dxccEntity)
 }
