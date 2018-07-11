f55dde3a22c5460352f53efff7828ff4445eb8a8
kecramer <kcramer@gmail.com>
2018-07-11 14:00:46 -0700

    Add socket to push spots out as they come in

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express'),
       cors = require('cors'),
       app = express(),
       db = require('./model'),
-      controller = require('./controller')
-      spotWorker = require('./worker')
+      controller = require('./controller'),
+      http = require('http'),
+      server = http.createServer(app),
+      io = require('socket.io').listen(server)
+
+server.listen(process.env.PORT || 3000)
+
+const spotWorker = require('./worker')(io)
 
 app.use(cors())
 
 app.get('/spot', controller.spot.index)
 app.get('/dxcc/:call', controller.dxcc.show)
 app.get('/dxcc/:call/usa', controller.dxcc.showUSA)
-
-app.listen(process.env.PORT || 3000, () => {
-   console.log('Express started on port ' + (process.env.PORT || '3000'))
-})
--- worker/index.js
+++ worker/index.js
@@ -1,30 +1,33 @@
 const db = require('../model');
 
-let DXCluster = require('dxcluster')
+const DXCluster = require('dxcluster')
 
-conn = new DXCluster()
+module.exports = (io) => {
+  conn = new DXCluster()
 
-let opts = {
-  host: 'dxusa.net',
-  port: 7300,
-  loginPrompt: 'login:',
-  call: 'K3AM-5'
+  let opts = {
+    host: 'dxusa.net',
+    port: 7300,
+    loginPrompt: 'login:',
+    call: 'K3AM-5'
+  }
+
+  conn.connect(opts)
+    .then(() => {
+      conn.on('spot', (spot) => {
+        db.Spot.create(spot, (err, newSpot) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+          io.emit('spot', newSpot)
+        });
+      })
+      conn.on('message', (msg) => {
+        console.log('[MSG]:' + msg)
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
-
-conn.connect(opts)
-  .then(() => {
-    conn.on('spot', (spot) => {
-      db.Spot.create(spot, (err, newSpot) => {
-        if (err) {
-          console.log(err)
-          return
-        }
-      });
-    })
-    conn.on('message', (msg) => {
-      console.log(msg)
-    })
-  })
-  .catch((err) => {
-    console.log(err)
-  })
